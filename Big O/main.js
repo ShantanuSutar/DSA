80ea4457858309d73c81cfa44fa680b5ffb82096
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-09-10 15:12:50 +0530

    O(1)

--- Big O/main.js
+++ Big O/main.js
@@ -1,12 +1,21 @@
-const nemo = ['nemo'];
-const everyone = new Array(1000).fill('nemo');
+// const nemo = ['nemo'];
+// const everyone = new Array(1000).fill('nemo');
 
-function findNemo(array) {
-    for(let i=0; i < array.length; i++){
-        if(array[i] === 'nemo')
-        console.log('Found NEMO!')
-    }
+// function findNemo(array) {
+//     for(let i=0; i < array.length; i++){
+//         if(array[i] === 'nemo')
+//         console.log('Found NEMO!')
+//     }
 
+// }
+
+// findNemo(everyone);
+
+const boxes = [0,1,2,3,4,5,6];
+
+function logFirstTwoBoxes(boxes) {
+    console.log(boxes[0]); // O(1)
+    console.log(boxes[1]); // O(1)
 }
 
-findNemo(everyone);
+logFirstTwoBoxes(boxes);  // O(2)
