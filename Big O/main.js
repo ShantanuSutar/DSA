a7a73463f6cc9096c6fb567f453626f5a390cae7
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-09-10 15:09:38 +0530

    O(n)

--- Big O/main.js
+++ Big O/main.js
@@ -1,14 +1,12 @@
 const nemo = ['nemo'];
 const everyone = new Array(1000).fill('nemo');
-function findNemo(array) {
 
-    let t0 = performance.now();
+function findNemo(array) {
     for(let i=0; i < array.length; i++){
         if(array[i] === 'nemo')
         console.log('Found NEMO!')
     }
-    let t1 = performance.now();
-    console.log('Call to find Nemo took ' + (t1 -t0) + ' milliseconds')
+
 }
 
 findNemo(everyone);
