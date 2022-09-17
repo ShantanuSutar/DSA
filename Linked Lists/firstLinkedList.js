723941c54d2b665cb4a32e3af86d292df339edb0
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-09-17 17:31:00 +0530

    printList method
    Printing in an array form

--- Linked Lists/firstLinkedList.js
+++ Linked Lists/firstLinkedList.js
@@ -1,4 +1,4 @@
-// 9 -->  10 --> 5  --> 16
+// 9 -->  10 --> 99 --> 5  --> 16
 
 // let myLinkedList = {
 //     head: {
@@ -47,6 +47,18 @@ class LinkedList {
         this.head = newNode;
         this.length++;
         return console.log(this);
+    }
+
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return console.log(array);
+    };
+    insert(index, value) {
 
     }
 }
@@ -55,4 +67,6 @@ const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(9);
+myLinkedList.printList();
+//myLinkedList.insert(2, 99);
 //console.log(myLinkedList);
