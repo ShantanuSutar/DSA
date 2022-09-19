01fd2dfcc8e535a266728eb49cbd6893106caba6
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-09-19 16:39:02 +0530

    DoublyLinkedList
    Also updating the methods - append, prepend and insert

---
+++ Linked Lists/doublyLinkedList.js
@@ -0,0 +1,100 @@
+class DoublyLinkedList {
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null,
+            prev: null
+        }
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    append(value) {
+        const newNode = {
+            value: value,
+            next: null,
+            prev: null
+        };
+        newNode.prev = this.tail;
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return console.log(this);
+    }
+
+
+    prepend(value) {
+        const newNode = {
+            value: value,
+            next: null,
+            prev: null
+        };
+        //   this.head.next = this.head;
+        newNode.next = this.head;
+        this.head.prev = newNode;
+        this.head = newNode;
+        this.length++;
+        return console.log(this);
+    }
+
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return console.log(array);
+    };
+    insert(index, value) {
+        //check params
+        if (index === 0) {
+            this.prepend(value);
+            return this.printList();
+        }
+        if (index >= this.length) {
+            this.append(value);
+            return this.printList();
+        }
+        const newNode = {
+            value: value,
+            next: null,
+            prev: null
+        };
+        const leader = this.traverseToIndex(index - 1);
+        const follower = leader.next;
+        leader.next = newNode;
+        newNode.prev = leader;
+        newNode.next = follower;
+        follower.prev = newNode;
+        this.length++;
+        return this.printList();
+    }
+    traverseToIndex(index) {
+        //check params 
+        let counter = 0;
+        let currentNode = this.head;
+        while (counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
+    remove(index) {
+        //check params 
+        const leader = this.traverseToIndex(index - 1);
+        const unwantedNode = leader.next;
+        leader.next = unwantedNode.next;
+        this.length--;
+        return this.printList();
+    }
+}
+
+const myLinkedList = new DoublyLinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(9);
+myLinkedList.insert(3, 99);
+myLinkedList.insert(30, 88);
+myLinkedList.insert(0, 7);
+// myLinkedList.remove(2);
